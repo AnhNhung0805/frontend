7a659fdd5f6ff8ba068e2972d1683970ed0ca94d
ADMIN <ADMIN@LAPTOP-BA2AQUNN>
2020-11-23 14:01:20 +0700

    anhlptgcd18651

---
+++ script.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: [" DEV Fullstack", " Dev Machine Learning", "DEV Fullstack", "Designer", "Freelancer"],
+        typeSpeed: 120,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["...loading", " My name is Le Phan Tuan Anh, I am currently studying 3rd year at FPT Greenwich. My hobby is to travel in many places, to many lands to know the culture, people, scenery. I also like programming like Dev Websitie, Machine Learning, Deep, ... As a photo lover, I love the beauty of the art of photography, ... Hi thanks for watching things about me."],
+        typeSpeed: 120,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
